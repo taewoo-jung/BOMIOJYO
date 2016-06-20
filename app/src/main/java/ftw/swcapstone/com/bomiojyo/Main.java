e32f19e63e1e7104f79dab2d292a9868ac410bfd
tw <xodn6734@ajou.ac.kr>
2016-06-20 17:45:26 +0900

    Test

---
+++ app/src/main/java/ftw/swcapstone/com/bomiojyo/Main.java
@@ -0,0 +1,80 @@
+package ftw.swcapstone.com.bomiojyo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.skp.Tmap.TMapView;
+
+import ftw.swcapstone.com.bomiojyo.map.SearchLocation;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //API KEY 설정
+        TMapView tmapView = new TMapView(this);
+        setAppKey(tmapView);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v){
+        int id = v.getId();
+
+        switch (id){
+            case R.id.ROUTE_SEARCH_BTN:{
+
+                startActivity(new Intent(this, SearchLocation.class));
+            }
+            default:
+                break;
+        }
+    }
+
+    public void setAppKey(TMapView tMapView){
+        tMapView.setSKPMapApiKey("5f1b1ac2-1131-395d-9555-d6fff8eb3e4e");
+    }
+}
